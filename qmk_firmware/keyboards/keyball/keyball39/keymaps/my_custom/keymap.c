b42017aa95925a2ef88a0e95182e080e1c72cc57
da1channn <da1channnit@gmail.com>
2025-06-11 22:29:22 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/keymap.c
@@ -120,29 +120,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
   ),
   
-  // Layer 1 - Nav/Mouse
   [1] = LAYOUT_universal(
-    KC_INS   , KC_HOME  , KC_UP    , KC_END   , KC_DEL   ,                            XXXXXXX  , KC_BTN3  , KC_MS_U  , KC_WH_U  , XXXXXXX  ,
-    KC_LCTL  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_LALT  ,                            XXXXXXX  , KC_BTN1  , KC_MS_L  , KC_MS_D  , KC_MS_R  ,
-    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                            XXXXXXX  , KC_BTN2  , KC_WH_L  , KC_WH_D  , KC_WH_R  ,
-    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
+    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
+    KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
   ),
-  
-  // Layer 2 - Numpad/F-Keys（JIS対応）
+
   [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , JIS_MINS ,                            XXXXXXX  , KC_F1    , KC_F2    , KC_F3    , KC_F4    ,
-    JIS_ASTR , KC_4     , KC_5     , KC_6     , JIS_PLUS ,                            XXXXXXX  , KC_F5    , KC_F6    , KC_F7    , KC_F8    ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     , JIS_EQL  ,                            XXXXXXX  , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
-    _______ , KC_0     , KC_0     , KC_DOT   , _______ , KC_ENT  , KC_BSPC , _______ , _______ , _______ , _______ , _______
+    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
+   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
   ),
-  
-  // Layer 3 - Symbols（JIS対応）
+
   [3] = LAYOUT_universal(
-    JIS_LBRC , JIS_RBRC , JIS_UNDS , JIS_BSLS , XXXXXXX  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    JIS_LCBR , JIS_RCBR , JIS_COLN , JIS_AT   , XXXXXXX  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    JIS_HASH , JIS_TILD , JIS_CIRC , XXXXXXX  , XXXXXXX  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    QK_BOOT  , QK_BOOT  , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
-  )
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+  ),
 };
 
 // Mod-Tapのタイミング調整
