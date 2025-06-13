8594da0da9849f322004886945fbf0f3a363536f
da1channn <da1channnit@gmail.com>
2025-06-13 12:00:19 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
@@ -1,25 +1,24 @@
 #pragma once
 
-// via/remap設定
+// ⭐ これが必須！KBキーコードを動作させるため
+#include "../../config.h"
+
+// 以下、カスタム設定
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
-// combo設定
 #define COMBO_COUNT 16
 #define COMBO_TERM 40
 #define COMBO_MUST_TAP_PER_COMBO
 #define COMBO_STRICT_TIMER
 
-// Mod-Tap設定（中指・人差し指に最適化）
 #define TAPPING_TERM 1000
 #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM 100
-// #define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD  // 無効のまま
 
-// Auto Mouse Keys設定
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 #define AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
 #define AML_ACTIVATE_THRESHOLD 70
 
-// トラックボール設定
 #define POINTING_DEVICE_TASK_THROTTLE_MS 8
