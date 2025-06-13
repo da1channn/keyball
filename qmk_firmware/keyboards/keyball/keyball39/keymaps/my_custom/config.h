84dc3bff0dfdcfa4c28dc0c7af8e0380f7b968a8
da1channn <da1channnit@gmail.com>
2025-06-13 12:32:42 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/config.h
@@ -1,19 +1,31 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
-// RGB効果を必要最小限に削減
+// RGB効果（容量削減のため最小限）
 #ifdef RGBLIGHT_ENABLE
-    // 基本効果のみ残す
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    // 以下はコメントアウトして無効化
-// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #    define RGBLIGHT_EFFECT_SNAKE
-// #    define RGBLIGHT_EFFECT_KNIGHT
-// #    define RGBLIGHT_EFFECT_CHRISTMAS
-// #    define RGBLIGHT_EFFECT_RGB_TEST
-// #    define RGBLIGHT_EFFECT_ALTERNATING
-// #    define RGBLIGHT_EFFECT_TWINKLE
+// 必要に応じて以下を有効化
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #endif
 
 #define TAP_CODE_DELAY 5
@@ -22,6 +34,9 @@
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
+// VIA用の動的レイヤー数
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
 // コンボ設定
 #define COMBO_COUNT 16
 #define COMBO_TERM 40
@@ -29,6 +44,6 @@
 #define COMBO_STRICT_TIMER
 
 // タッピング設定
-#define TAPPING_TERM 800
+#define TAPPING_TERM 1000
 #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM 100
