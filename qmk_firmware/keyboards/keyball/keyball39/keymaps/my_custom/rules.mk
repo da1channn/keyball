629c173948e0f491344edd03613ac4ca0ce9afea
da1channn <da1channnit@gmail.com>
2025-06-13 10:32:05 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/rules.mk
@@ -1,8 +1,9 @@
-# Keyballライブラリを明示的に追加（これが重要！）
-SRC += $(KEYBOARD_PATH_3)/lib/keyball/keyball.c
+# Keyball39 カスタムキーマップ rules.mk - ベスト版
+# VIAキーマップと同じ設定を使用（最も確実）
 
-# カスタム設定
-RGBLIGHT_ENABLE = yes
-OLED_ENABLE = yes
 VIA_ENABLE = yes
+TAP_DANCE_ENABLE = no
 COMBO_ENABLE = yes
+
+# 以上！これだけで全機能が動作します
+# OLED、RGB、Keyballライブラリは親から自動継承
