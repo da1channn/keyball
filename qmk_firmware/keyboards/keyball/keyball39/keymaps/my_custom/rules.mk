dda24618b2135e1a91c1b63ab93a3f335fdd3a41
da1channn <da1channnit@gmail.com>
2025-06-13 11:07:04 +0900

    Update rules.mk

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/my_custom/rules.mk
@@ -1,9 +1,3 @@
-# Keyball39 カスタムキーマップ rules.mk - ベスト版
-# VIAキーマップと同じ設定を使用（最も確実）
-
 VIA_ENABLE = yes
 TAP_DANCE_ENABLE = no
 COMBO_ENABLE = yes
-
-# 以上！これだけで全機能が動作します
-# OLED、RGB、Keyballライブラリは親から自動継承
